e66a6c5f08fc5ba351396a73033686a98eee4961
vikash saini <vickykartoos1598@gmail.com>
2022-10-31 23:52:21 +0530

    first commit

---
+++ login_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+# Register your models here.
+
+
+# class RegisterAdmin(admin.ModelAdmin):
+#     list_display= ['id', 'username', 'email', 'password']
+#     # search_fields = ['name']
+# admin.site.register(User, RegisterAdmin)
