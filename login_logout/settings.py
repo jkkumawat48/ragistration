e31e3be04e93e564304215fc31e93f4c009d1a58
jkkumawat48 <110739546+jkkumawat48@users.noreply.github.com>
2022-11-07 21:26:05 +0530

    Update settings.py

--- login_logout/settings.py
+++ login_logout/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -97,17 +97,10 @@ CORS_ALLOW_HEADERS = (
 
 
 
-# CORS_ORIGIN_WHITELIST = (
-# #     'http://localhost:8000',
-# #     'http://127.0.0.1:9000',
-# #     'http://localhost:3000',
-# #     'http://127.0.0.1:3000',
-#     'http://127.0.0.1:4200',
-#     'https://ragister-api.herokuapp.com',
-#     # 'https://ilearnplacedev.netlify.app',
-#     # 'https://ilearnplace.netlify.app',
-#     # 'https://ilearnplace.com',
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:4200',
+    'https://ragister-api.herokuapp.com',
+)
 
 CORS_ALLOW_METHODS = (
     'DELETE',
